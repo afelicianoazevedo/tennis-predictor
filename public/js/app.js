5d0fbef13d2c2097578f73a6a81d4dabe20056c4
afelicianoazevedo <a.feliciano.azevedo@gmail.com>
2026-08-23 01:04:05 +0100

    fix: use correct PostgREST filter syntax (col=op.value)

--- public/js/app.js
+++ public/js/app.js
@@ -13,9 +13,9 @@ function log(msg) {
 async function api(table, opts = {}) {
     const { select = '*', eq = {}, gte = {}, lte = {}, order = 'scheduled_at.asc', limit = 100 } = opts;
     const p = new URLSearchParams({ select, order, limit: limit.toString() });
-    Object.entries(eq).forEach(([k, v]) => p.set(k, `eq.${v}`));
-    Object.entries(gte).forEach(([k, v]) => p.set(k, `gte.${v}`));
-    Object.entries(lte).forEach(([k, v]) => p.set(k, `lte.${v}`));
+    Object.entries(eq).forEach(([k, v]) => p.set(`${k}=eq.${v}`));
+    Object.entries(gte).forEach(([k, v]) => p.set(`${k}=gte.${v}`));
+    Object.entries(lte).forEach(([k, v]) => p.set(`${k}=lte.${v}`));
 
     const url = `${SUPABASE_URL}/rest/v1/${table}?${p}`;
     log('Fetching: ' + url);
